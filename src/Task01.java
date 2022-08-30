7805c2aa5c2f9d6b7722763fcbd53ca2d507dcd8
yalo24 <olgarizkovic@gmail.com>
2022-08-30 11:10:25 +0300

    Initial commit

---
+++ src/Task01.java
@@ -0,0 +1,10 @@
+public class Task01 {
+    public static void main(String[] args) {
+        int counter = 5;
+        while (counter > 0) {
+
+            System.out.println("Hello!");
+            counter--;
+        }
+    }
+}
